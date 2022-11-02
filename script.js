be96dbe6aa50f4cfad6a314c3b6624127e8ebe0a
Vlad Kirichuk <vlad.kirichuk50@gmail.com>
2022-11-02 14:52:02 +0300

    Add infinity animation loop in css.file

---
+++ script.js
@@ -0,0 +1,18 @@
+var myMusic = document.getElementById("music");
+
+function play() {
+    myMusic.play;
+}
+
+function pause() {
+    myMusic.pause;
+}
+
+var button_sendMessage = getElementById("blue");
+var telegram_link = getElementById("telegram");
+
+button_sendMessage.onclick = function() {
+    button_sendMessage.onclick;
+}
+
+
